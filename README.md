b6c35d69b27a348b209de58dcff501a037d1dce7
Cyanide <yeety150@gmail.com>
2022-12-10 14:04:33 -0800

    End screen changes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Systems-Check
-Program that runs on startup to display a check on all systems
+# Flight computer for a raspberry pi
+SystemsCheck.py - Program that runs on startup to display a check on all systems
--- SystemsCheck.py
+++ SystemsCheck.py
@@ -33,25 +33,27 @@ def updateStatus():
         w.after(500, updateStatus)
 
 def systemComplete():
-    f1 = Frame(w, width = 576+3, height = 70+3, highlightbackground="#75fb4c", highlightthickness=3)
-    f1.place(relx=0.5, rely=0.5, anchor=CENTER)
+    f1 = Frame(w, width = 10, height = 10, highlightbackground="#75fb4c", highlightthickness=3)
     l1 = Label(w, text="Systems Check Complete", bg="black", fg="#75fb4c", font="verdana 20 bold", width=30, height=2)
+    f1['width'] = 456+6
+    f1['height'] = 54+6
+    f1.place(relx=0.5, rely=0.5, anchor=CENTER)
     l1.place(relx=0.5, rely=0.5, anchor=CENTER)
-    print(l1.winfo_reqheight())
+    print(l1.winfo_reqwidth())
 
 w = Tk()
 w.title("Systems Check")
 w.configure(background='black')
 screenWidth = w.winfo_screenwidth()
 screenHeight = w.winfo_screenheight()
-w.geometry("%dx%d+%d+%d" % (screenWidth, screenHeight, -10, 0))
+w.geometry("%dx%d+%d+%d" % (screenWidth, screenHeight, 0, 0))
 
 checkListSize = 10
 s1 = [Label] * checkListSize
 
 for i in range(checkListSize):
     s1[i] = Label(w, text="System Name: Checking...", bg="black", fg="white", font="verdana 20 bold")
-    s1[i].place(x=0, y=38 * i)
+    s1[i].place(x=0, y=s1[i].winfo_reqheight() * i)
 
 w.after(500, updateStatus)
 w.after(500 * (checkListSize + 2), systemComplete)
