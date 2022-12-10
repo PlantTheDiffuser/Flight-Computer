9e3357821f07fd0e65940fc26c6f600b73ea2731
Ikshul Bethur <nickshul5@gmail.com>
2022-12-09 23:15:21 -0800

    Running animation of system check

--- UserOutput.py
+++ UserOutput.py
@@ -1,5 +1,39 @@
 from tkinter.ttk import Progressbar
 from tkinter import *
+import random
+
+class Unbuffered(object):
+   def __init__(self, stream):
+       self.stream = stream
+   def write(self, data):
+       self.stream.write(data)
+       self.stream.flush()
+   def writelines(self, datas):
+       self.stream.writelines(datas)
+       self.stream.flush()
+   def __getattr__(self, attr):
+       return getattr(self.stream, attr)
+
+import sys
+sys.stdout = Unbuffered(sys.stdout)
+
+counter = 0
+
+def updateStatus():
+    global counter
+    rand = random.randint(0, 100)
+    if rand < 50:
+        s1[counter]['text'] = "System Name: Offline"
+        #s1[counter]['color'] = "red"
+    else:
+        s1[counter]['text'] = "System Name: Online"
+    counter = counter + 1
+    if counter < checkListSize:
+        w.after(1000, updateStatus)
+
+def systemComplete():
+    l1 = Label(w, text="Systems Check Complete", bg="Green", fg="black", font="verdana 20 bold", width=30, height=2)
+    l1.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 w = Tk()
 w.title("Systems Check")
@@ -8,15 +42,15 @@ screenWidth = w.winfo_screenwidth()
 screenHeight = w.winfo_screenheight()
 w.geometry("%dx%d+%d+%d" % (screenWidth, screenHeight, -10, 0))
 
-
-checkListSize = 5
-
-statusHeight = screenHeight / checkListSize
-
+checkListSize = 10
+s1 = [Label] * checkListSize
 
 for i in range(checkListSize):
-    ypos = statusHeight + (i * screenHeight)
-    s1 = Label(w, text="Status " + str(i), bg="black", fg="white", font=("Helvetica", 16))
-    s1.place(x=0, y=ypos)
+    s1[i] = Label(w, text="System Name: Checking...", bg="black", fg="white", font="verdana 20 bold")
+    s1[i].place(x=0, y=38 * i)
+
+w.after(1000, updateStatus)
+w.after(1000 * (checkListSize + 1), systemComplete)
+w.after(1000 * (checkListSize + 2), w.destroy)
 
 w.mainloop()
