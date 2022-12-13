af8534d1a03c4e9f550c7322455675029e977ffc
Cyanide <yeety150@gmail.com>
2022-12-12 22:00:05 -0800

    Added basic logic

--- RearViewCamera.py
+++ RearViewCamera.py
@@ -1,19 +1,21 @@
 from gpiozero import Servo
 from time import sleep
 
-
-
+#Called only by SystemsCheck.py on startup
 def statusCheck():
     return False
-
 name = "Rear View Camera"
+#--
 
 
 servo = Servo(25)
+rightSignal = "pin 15"
+leftSignal = "pin 14"
 
-servo.min()
-sleep(0.5)
+if rightSignal == "Hi" :
+    servo.max()
+    sleep(5)
+if leftSignal == "Hi" :
+    servo.min()
+    sleep(5)
 servo.mid()
-sleep(0.5)
-servo.max()
-sleep(0.5)
