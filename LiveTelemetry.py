4524dad2752b8c713e49cfa56367e5e9c82158ce
Ikshul Bethur <nickshul5@gmail.com>
2022-12-13 19:49:01 -0800

    idek gitub copilot took care of most things

--- LiveTelemetry.py
+++ LiveTelemetry.py
@@ -104,16 +104,16 @@ def updateDeltaTime():
 w = tk.Tk()
 w.title("Systems Check")
 w.configure(background='black')
-screenWidth = w.winfo_screenwidth()
-screenHeight = w.winfo_screenheight()
-w.geometry("%dx%d+%d+%d" % (screenWidth, screenHeight, -10, 0))
+screenWidth = 800#w.winfo_screenwidth()
+screenHeight = 480#w.winfo_screenheight()
+w.geometry("%dx%d+%d+%d" % (screenWidth, screenHeight, 0, 0))
 
 #Pedal position indicators
-throttle = ttk.Progressbar(w, orient='vertical', length=290, mode='determinate', value=0)
-brake = ttk.Progressbar(w, orient='vertical', length=290, mode='determinate', value=0)
+throttle = ttk.Progressbar(w, orient='vertical', length=200, mode='determinate', value=0)
+brake = ttk.Progressbar(w, orient='vertical', length=200, mode='determinate', value=0)
 barSpacing = 10
 PPIxRef = screenWidth - (2 * (barSpacing + throttle.winfo_reqwidth()))
-PPIyRef = screenHeight - ((2 * barSpacing) + throttle.winfo_reqheight() + 35)
+PPIyRef = screenHeight - (barSpacing + throttle.winfo_reqheight())
 
 brake.place(x = PPIxRef, y = PPIyRef)
 throttle.place(x = PPIxRef + barSpacing + throttle.winfo_reqwidth(), y = PPIyRef)
@@ -129,8 +129,6 @@ lastLap = tk.Label(w, text="LAST --:--.---", bg="black", fg="#FFFFFF", font="hel
 bestLap = tk.Label(w, text="BEST --:--.---", bg="black", fg="#FFFFFF", font="helvetica 24 bold", width=16, height=1)
 currentLap = tk.Label(w, text="--:--.---", bg="black", fg="#FFFFFF", font="helvetica 42 bold", width=9, height=1)
 
-
-
 LapTimeFrame['width'] = race.winfo_reqwidth() + lap.winfo_reqwidth() + 6
 LapTimeFrame.place(x = -3, y = -3)
 race.place(x = 0, y = 0)
@@ -141,7 +139,22 @@ bestLap.place(x = 0, y = race.winfo_reqheight() + deltaTime.winfo_reqheight() + 
 currentLap.place(x = 0, y = LapTimeFrame.winfo_reqheight() - (6 + currentLap.winfo_reqheight()))
 w.after(1, updateDeltaTime)
 
+#Vehicle information
+gear = tk.Label(w, text="N", bg="black", fg="#FFFFFF", font="helvetica 100 bold")
+revs = tk.Label(w, text="-----", bg="black", fg="#FFFFFF", font="helvetica 80 bold")
+brakeBal = tk.Label(w, text="BRAKE  62%", bg="black", fg="#FFFFFF", font="helvetica 24 bold")
+speed = tk.Label(w, text="---", bg="black", fg="#FFFFFF", font="helvetica 36 bold")
+drs = tk.Label(w, text="DRS       CLOSED", bg="black", fg="#FFFFFF", font="helvetica 24 bold")
+strat = tk.Label(w, text="STRAT   6", bg="black", fg="#FFFFFF", font="helvetica 24 bold")
 
+gear.place(x = LapTimeFrame.winfo_reqwidth() + deltaTime.winfo_reqheight(), y = (LapTimeFrame.winfo_reqheight() - gear.winfo_reqheight()) / 2)
+revs.place(x = LapTimeFrame.winfo_reqwidth() + deltaTime.winfo_reqheight() + gear.winfo_reqheight(), y = (LapTimeFrame.winfo_reqheight() - revs.winfo_reqheight()) / 2)
+brakeBal.place(x = 10, y = LapTimeFrame.winfo_reqheight())
+speed.place(x = screenWidth - speed.winfo_reqwidth() - 10, y = 0)
+drs.place(x = 10, y = LapTimeFrame.winfo_reqheight() + brakeBal.winfo_reqheight())
+strat.place(x = 10, y = LapTimeFrame.winfo_reqheight() + brakeBal.winfo_reqheight() + strat.winfo_reqheight())
+
+#Temporary lap button for testing. Actual button will be on steering wheel
 lapbutton = tk.Button(w, text="LAP", command=updateLap, bg="black", fg="#FFFFFF", font="helvetica 24 bold", width=8, height=1)
 lapbutton.pack()
 
--- SetUp.py
+++ SetUp.py
@@ -1,4 +1,7 @@
 #Variables for setup
+#Detailed explination in Setup Guide
 
 TotalLaps = 10 #0 if not racing
 qualifying = 0 #0 if not qualifying, 1, 2, or 3 for Q1, Q2, or Q3
+strat = 6 #Strats explained in Setup Guide
+brakeBias = 62
